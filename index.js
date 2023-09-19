87047157bba234723edab28a060e525787a6a359
mostakshahariyar <mostakshahariyar18@gmail.com>
2023-09-19 18:04:07 +0600

    port will be costomize

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
